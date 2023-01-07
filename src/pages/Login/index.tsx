b85cb4b3f509439a934386bb88b3636bec01ad35
WOo0W <24245520+WOo0W@users.noreply.github.com>
2023-01-07 09:13:05 +0000

    login: add noise for dark mode to reduce banding

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -8,6 +8,7 @@ import {
   Stack,
   TextField,
   Typography,
+  useColorScheme,
 } from '@mui/joy'
 import { AxiosError } from 'axios'
 import { useState } from 'react'
@@ -15,6 +16,76 @@ import { useNavigate } from 'react-router-dom'
 import { http, UserType } from '../../http'
 import { cyan, pink, purple } from '@mui/material/colors'
 import { alpha } from '@mui/material'
+import noise from './noise.svg'
+
+const Background = () => {
+  const fixed = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  }
+  const { colorScheme } = useColorScheme()
+  return (
+    <>
+      <Box
+        sx={(theme) => ({
+          ...fixed,
+          zIndex: -5,
+
+          backgroundImage: `
+            linear-gradient(
+              180deg,
+              rgba(${theme.vars.palette.neutral.mainChannel} / 0.01),
+              rgba(${theme.vars.palette.primary.mainChannel} / 0.15) 100%
+            ),
+            radial-gradient(
+              ellipse at top left,
+              rgba(${theme.vars.palette.primary.mainChannel} / 0.3),
+              transparent 50%
+            ),
+            radial-gradient(
+              ellipse at top right,
+              ${alpha(cyan[300], 0.3)},
+              transparent 50%
+            ),
+            radial-gradient(
+              ellipse at center right,
+              ${alpha(purple[300], 0.3)},
+              transparent 55%
+            ),
+            radial-gradient(
+              ellipse at center left,
+              ${alpha(pink[300], 0.2)},
+              transparent 50%
+            )`,
+        })}
+      />
+      <Box
+        sx={{
+          ...fixed,
+          zIndex: -4,
+          backdropFilter: 'blur(48px)',
+        }}
+      />
+      {colorScheme === 'dark' && (
+        // Noise layer to reduce banding
+        // In the dark mode, the banding is more obvious
+        <Box
+          sx={{
+            ...fixed,
+            zIndex: -3,
+            background: `
+              url(${noise})
+            `,
+            mixBlendMode: 'multiply',
+          }}
+        />
+      )}
+    </>
+  )
+}
 
 export const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -49,43 +120,7 @@ export const Login = () => {
 
   return (
     <>
-      <Box
-        sx={(theme) => ({
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          zIndex: -1,
-
-          backgroundImage: `
-            linear-gradient(
-              180deg,
-              rgba(${theme.vars.palette.neutral.mainChannel} / 0.01),
-              rgba(${theme.vars.palette.primary.mainChannel} / 0.1) 100%
-            ),
-            radial-gradient(
-              ellipse at top left,
-              rgba(${theme.vars.palette.primary.mainChannel} / 0.3),
-              transparent 50%
-            ),
-            radial-gradient(
-              ellipse at top right,
-              ${alpha(cyan[300], 0.3)},
-              transparent 50%
-            ),
-            radial-gradient(
-              ellipse at center right,
-              ${alpha(purple[300], 0.3)},
-              transparent 55%
-            ),
-            radial-gradient(
-              ellipse at center left,
-              ${alpha(pink[300], 0.3)},
-              transparent 50%
-            )`,
-        })}
-      />
+      <Background />
       <Container
         maxWidth={false}
         sx={{
@@ -94,7 +129,6 @@ export const Login = () => {
           alignItems: 'center',
           justifyContent: 'center',
           height: '95vh',
-          backdropFilter: 'blur(48px)',
         }}
       >
         <Card
